d1d159821366335d4e91036c79df281e8d1d9f9b
Wesley Reisz <wes@wesleyreisz.com>
2014-12-02 13:29:03 -0500

    added login structure... commented out fb login because of configuration issues

--- mobile/src/main/java/com/wesleyreisz/notmymusic/activity/BaseActivity.java
+++ mobile/src/main/java/com/wesleyreisz/notmymusic/activity/BaseActivity.java
@@ -2,8 +2,12 @@ package com.wesleyreisz.notmymusic.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.wesleyreisz.notmymusic.Constants;
+import com.wesleyreisz.notmymusic.R;
 
 /**
  * Created by wesleyreisz on 12/2/14.
@@ -15,7 +19,10 @@ public class BaseActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Checking login");
-        if(false){
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.PREFS_NAME, 0);
+
+        String uid = sharedPreferences.getString(Constants.LOGIN_FIELD, "na");
+        if(uid==null || "na".equalsIgnoreCase(uid)){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
--- mobile/src/main/java/com/wesleyreisz/notmymusic/activity/LoginActivity.java
+++ mobile/src/main/java/com/wesleyreisz/notmymusic/activity/LoginActivity.java
@@ -1,40 +1,66 @@
 package com.wesleyreisz.notmymusic.activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.wesleyreisz.notmymusic.Constants;
 import com.wesleyreisz.notmymusic.R;
 
 public class LoginActivity extends Activity {
+    private static final String TAG = "LOGIN SONG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login_activity_menu, menu);
-        return true;
+        Parse.initialize(this, Constants.APP_ID, Constants.APP_SECRET);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStart() {
+        super.onStart();
+        ParseFacebookUtils.initialize(String.valueOf(R.string.facebook_app_id));
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        ImageView faceBookButton = (ImageView)findViewById(R.id.imageViewFacebook);
+        faceBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                ParseFacebookUtils.logIn(LoginActivity.this, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException err) {
+                        if (user == null) {
+                            Log.d(TAG, "Uh oh. The user cancelled the Facebook login.");
 
-        return super.onOptionsItemSelected(item);
+                        } else if (user.isNew()) {
+                            Log.d(TAG, "User signed up and logged in through Facebook!");
+                        } else {
+                            Log.d(TAG, "User logged in through Facebook!");
+                            SharedPreferences sharedPreferences = getSharedPreferences(Constants.PREFS_NAME, 0);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString(Constants.LOGIN_FIELD, user.getUsername());
+                        }
+                    }
+                });
+                */
+                SharedPreferences sharedPreferences = getSharedPreferences(Constants.PREFS_NAME, 0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(Constants.LOGIN_FIELD, "wes");
+                finish();
+            }
+        });
     }
 }
